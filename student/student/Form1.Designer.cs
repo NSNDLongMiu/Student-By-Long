7780bd4559d6dd8350113f8029917c9938392bc2
longsieunhan <62877217+longsieunhan@users.noreply.github.com>
2020-05-09 17:42:23 +0700

    first commit

---
+++ student/student/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace student_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_lop = new System.Windows.Forms.TextBox();
+            this.txt_tensv = new System.Windows.Forms.TextBox();
+            this.txt_masv = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_luu = new System.Windows.Forms.Button();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.btn_sua = new System.Windows.Forms.Button();
+            this.txt_timkiem = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_diem = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_diem);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_lop);
+            this.groupBox1.Controls.Add(this.txt_tensv);
+            this.groupBox1.Controls.Add(this.txt_masv);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(491, 134);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin sinh viên";
+            // 
+            // txt_lop
+            // 
+            this.txt_lop.Location = new System.Drawing.Point(106, 83);
+            this.txt_lop.Name = "txt_lop";
+            this.txt_lop.Size = new System.Drawing.Size(365, 20);
+            this.txt_lop.TabIndex = 5;
+            // 
+            // txt_tensv
+            // 
+            this.txt_tensv.Location = new System.Drawing.Point(106, 57);
+            this.txt_tensv.Name = "txt_tensv";
+            this.txt_tensv.Size = new System.Drawing.Size(365, 20);
+            this.txt_tensv.TabIndex = 4;
+            // 
+            // txt_masv
+            // 
+            this.txt_masv.Location = new System.Drawing.Point(106, 31);
+            this.txt_masv.Name = "txt_masv";
+            this.txt_masv.Size = new System.Drawing.Size(365, 20);
+            this.txt_masv.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên sinh viên:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lớp : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã sinh viên:";
+            // 
+            // btn_luu
+            // 
+            this.btn_luu.Location = new System.Drawing.Point(118, 192);
+            this.btn_luu.Name = "btn_luu";
+            this.btn_luu.Size = new System.Drawing.Size(95, 32);
+            this.btn_luu.TabIndex = 7;
+            this.btn_luu.Text = "Lưu";
+            this.btn_luu.UseVisualStyleBackColor = true;
+            this.btn_luu.Click += new System.EventHandler(this.btn_luu_Click);
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.Location = new System.Drawing.Point(388, 192);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(95, 32);
+            this.btn_xoa.TabIndex = 8;
+            this.btn_xoa.Text = "Xóa";
+            this.btn_xoa.UseVisualStyleBackColor = true;
+            this.btn_xoa.Click += new System.EventHandler(this.btn_xoa_Click);
+            // 
+            // btn_sua
+            // 
+            this.btn_sua.Location = new System.Drawing.Point(253, 192);
+            this.btn_sua.Name = "btn_sua";
+            this.btn_sua.Size = new System.Drawing.Size(95, 32);
+            this.btn_sua.TabIndex = 9;
+            this.btn_sua.Text = "Sửa";
+            this.btn_sua.UseVisualStyleBackColor = true;
+            this.btn_sua.Click += new System.EventHandler(this.btn_sua_Click);
+            // 
+            // txt_timkiem
+            // 
+            this.txt_timkiem.Location = new System.Drawing.Point(118, 241);
+            this.txt_timkiem.Name = "txt_timkiem";
+            this.txt_timkiem.Size = new System.Drawing.Size(365, 20);
+            this.txt_timkiem.TabIndex = 7;
+            this.txt_timkiem.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_timkiem_KeyUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tìm kiếm:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 281);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(475, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // txt_diem
+            // 
+            this.txt_diem.Location = new System.Drawing.Point(106, 109);
+            this.txt_diem.Name = "txt_diem";
+            this.txt_diem.Size = new System.Drawing.Size(365, 20);
+            this.txt_diem.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Điểm";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 453);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_timkiem);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_sua);
+            this.Controls.Add(this.btn_xoa);
+            this.Controls.Add(this.btn_luu);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_lop;
+        private System.Windows.Forms.TextBox txt_tensv;
+        private System.Windows.Forms.TextBox txt_masv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_luu;
+        private System.Windows.Forms.Button btn_xoa;
+        private System.Windows.Forms.Button btn_sua;
+        private System.Windows.Forms.TextBox txt_timkiem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_diem;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ student/student/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace student_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }       
+        tbl_sinhvien tb = new tbl_sinhvien();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext();
+            var Lst = (from s in db.tbl_sinhviens select s).ToList();
+            dataGridView1 .DataSource = Lst;
+            txt_masv.DataBindings.Clear();
+            txt_tensv.DataBindings.Clear();
+            txt_lop.DataBindings.Clear();
+            txt_masv.DataBindings.Add("text", Lst, "masv");
+            txt_tensv.DataBindings.Add("text", Lst, "tensv");
+            txt_lop.DataBindings.Add("text", Lst, "lop");
+            txt_diem.DataBindings.Add("text", Lst, "diem");
+        }
+
+        private void btn_luu_Click(object sender, EventArgs e)
+        {            
+            DataContext db = new DataContext();
+            tb.masv = txt_masv .Text;
+            tb.tensv = txt_tensv .Text;
+            tb.lop = txt_lop.Text;
+            tb.diem = txt_diem.Text;
+            db.tbl_sinhviens .InsertOnSubmit (tb);
+            db.SubmitChanges();
+            Form1_Load(sender, e);
+
+        }
+
+        private void btn_sua_Click(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext();
+            tb = db.tbl_sinhviens.Where(s => s.masv == txt_masv.Text).Single();
+            tb.tensv = txt_tensv.Text;
+            tb.lop = txt_lop.Text;
+            tb.diem = txt_diem.Text;
+            db.SubmitChanges();
+            Form1_Load(sender, e);
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext();
+            tb = db.tbl_sinhviens.Where(s => s.masv == txt_masv.Text).Single();
+            tb.tensv = txt_tensv.Text;
+            tb.lop = txt_lop.Text;
+            tb.diem = txt_diem.Text;
+            db.tbl_sinhviens.DeleteOnSubmit(tb);
+            db.SubmitChanges();
+            Form1_Load(sender, e);
+        }
+
+        private void txt_timkiem_KeyUp(object sender, KeyEventArgs e)
+        {
+            DataContext db = new DataContext();
+            var Lst = (from s in db.tbl_sinhviens  where s.masv.Contains (txt_timkiem.Text ) select s ).ToList();
+            dataGridView1.DataSource = Lst;
+            txt_masv.DataBindings.Clear();
+            txt_tensv.DataBindings.Clear();
+            txt_lop.DataBindings.Clear();
+            txt_masv.DataBindings.Add("text", Lst, "masv");
+            txt_tensv.DataBindings.Add("text", Lst, "tensv");
+            txt_lop.DataBindings.Add("text", Lst, "lop");
+            txt_diem.DataBindings.Add("text", Lst, "diem");
+        }
+    }
+}
